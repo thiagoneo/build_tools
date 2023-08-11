bc23906fd8aed22a6b015831194a5df28898863e
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-08-11 15:17:24 -0300

    Update base.py

--- scripts/base.py
+++ scripts/base.py
@@ -444,9 +444,13 @@ def set_cwd(dir):
 def git_update(repo, is_no_errors=False, is_current_dir=False, git_owner=""):
   print("[git] update: " + repo)
   owner = git_owner if git_owner else "ONLYOFFICE"
+  if (repo == "server"):
+    owner = git_owner if git_owner else "thiagoneno"
   url = "https://github.com/" + owner + "/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
     url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    if (repo == "server"):
+      url = "git@github.com:thiagoneo/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
